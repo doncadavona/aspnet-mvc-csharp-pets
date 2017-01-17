b7c033eaece9f0d680418bfb3318a6ec3f35894d
doncadavona <dcadavona@gmail.com>
2017-01-18 01:26:48 +0800

    Initial controller for Screening Questions.

---
+++ PetSure2/Controllers/ScreeningQuestionsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PetSure2.Controllers
+{
+    public class ScreeningQuestionsController : Controller
+    {
+        // GET: ScreeningQuestions
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
