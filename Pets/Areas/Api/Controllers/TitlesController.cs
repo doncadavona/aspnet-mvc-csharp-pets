4bb4cb1b876324344d9218853e1c3fa1b5a47de6
doncadavona <dcadavona@gmail.com>
2017-01-18 08:25:26 +0800

    Initial titles api.

---
+++ Pets/Areas/Api/Controllers/TitlesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pets.Areas.Api.Controllers
+{
+    public class TitlesController : Controller
+    {
+        // GET: Api/Titles
+        public JsonResult Index()
+        {
+            var titles = new[]
+            {
+                new { id = 1, titleCode = "Code 1", title = "Pet 1" },
+                new { id = 1, titleCode = "Code 2", title = "Pet 2" },
+                new { id = 1, titleCode = "Code 3", title = "Pet 3" }
+            };
+            return Json(titles, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
